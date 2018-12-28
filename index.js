3d6af6ab92126239c26a292a04f3b9829e8a3496
Douddle <santa.mouse.re@mail.ru>
2018-12-28 16:10:45 +0300

    Fix rss

--- index.js
+++ index.js
@@ -51,14 +51,14 @@ client.on("message", async message => {
 
 client.on("message", async message => {
   if(message.content === "z.res") {
-    if(!message.author.id === "339462715917729792") {
-    message.channel.send("Доступ запрещен.");
+    if(message.author.id === "339462715917729792") {
+      let used = process.memoryUsage().rss / 1024 / 1024;
+      message.channel.send(`Приблизительное количество выделенных ресурсов составляет ${Math.round(used * 100) / 100} МБ`);
       } else {
-    let used = process.memoryUsage().rss / 1024 / 1024;
-    message.channel.send(`Приблизительное количество выделенных ресурсов составляет ${Math.round(used * 100) / 100} МБ`);
-  }
- }
-})
+    message.channel.send("Доступ запрещен.")
+  };
+ };
+});
 
 
 //
