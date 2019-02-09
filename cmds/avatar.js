ef7e4d9c0004197e1d25132f6a11d3b40e930b9e
mirrorLz <47481089+mirrorLz@users.noreply.github.com>
2019-02-09 20:13:57 +0300

    Avatar.js

---
+++ cmds/avatar.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+module.exports.run = (pult, message, args) => {
+    if (args.join(" ") == "") { //Начало.
+      let er = new Discord.RichEmbed() //Начало ембеда.
+      .setTitle("Ошибка")
+      .setColor("RANDOM")
+      .setDescription("Пользователь не был упомянут."); //Конец ембеда.
+        message.channel.send(er);
+        return;
+    } else {
+        let user = message.mentions.users.first(); //Пинг.
+        let image = user.displayAvatarURL; //Фото.
+        let embed = new Discord.RichEmbed() //Начало ембеда.
+            .setAuthor(`${user.username}#${user.discriminator}`)
+            .setColor("#ff0000")
+            .setImage(image); //Конец ембеда.
+        message.channel.send(embed); //Итог.
+    }
+}
+
+module.exports.help = {
+  name:"z.avatar"
+}
