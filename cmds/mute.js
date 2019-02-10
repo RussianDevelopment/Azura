d9e7b0a01849bfed3a00c0246ae5ad5c964f07d7
ripple <40640834+Comingo@users.noreply.github.com>
2019-02-10 12:21:47 +0300

    Mute.js

---
+++ cmds/mute.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+
+module.exports.run = (client, message, args) => 
+{
+  let role = message.guild.roles.find(role => role.name === "Main");
+  let mute = message.guild.roles.find(role => role.name === "AzuraMute");
+    let member = message.mentions.users.first()
+    if(message.member.roles.has(main.id))
+    {
+      if(message.member.roles.has(mute.id)) {
+        message.channel.send(`Вы уже проходили проверку, ${message.author.username}: роль присутствует.`)
+          } else {
+            member.addRole(verify).catch(console.error);
+        message.channel.send(`Проверка пройдена для ${message.author.username}: каналы открыты.`)
+          }
+     } else {
+      message.reply("недостаточно прав.")
+     } 
+
+};
+
+module.exports.help = {
+  name: "z.mute"
+}
