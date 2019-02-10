97d2421828ffa6c5a42461b9e4b92035f8d554c1
ripple <40640834+Comingo@users.noreply.github.com>
2019-02-10 12:44:25 +0300

    Update mute.js

--- cmds/mute.js
+++ cmds/mute.js
@@ -4,15 +4,15 @@ module.exports.run = (client, message, args) =>
 {
   let main = message.guild.roles.find(role => role.name === "Main");
   let mute = message.guild.roles.find(role => role.name === "AzuraMute");
-    let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let m = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(message.member.roles.has(main.id))
     {
-      if(message.member.roles.has(mute.id)) {
-            member.removeRole(mute.id);
-        message.channel.send(`${member} был размучен ${message.author}`)
+      if(m.roles.has(mute.id)) {
+            m.removeRole(mute.id);
+        message.channel.send(`${m} был размучен ${message.author}`)
           } else {
-            member.addRole(mute.id);
-        message.channel.send(`${member} был замучен ${message.author}`)
+            m.addRole(mute.id);
+        message.channel.send(`${m} был замучен ${message.author}`)
           }
      } else {
       message.reply("недостаточно прав.")
