846f268909a5cdab8989edc3aa866c4458d3f8f8
ripple <40640834+Comingo@users.noreply.github.com>
2019-02-12 16:47:41 +0300

    ALL XP

--- index.js
+++ index.js
@@ -106,6 +106,273 @@ client.on("message", async message => {
       });
 });
 
+//
+//
+//
+
+client.on("message", async message => {
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+
+  if(message.content === "z.take Начинающий") {
+    if(curxp > 500){
+      let coder = message.guild.roles.find(role => role.name === "Начинающий");
+      let member = message.member;
+      if(message.member.roles.has(coder.id)) {
+        message.channel.send(`Вы уже получали роль, ${message.author.username}`)
+      } else {
+        member.addRole(coder).catch(console.err);
+        message.channel.send(`Роль "Начинающий" выдана пользователю ${message.author.username}`)
+      };
+    } else {
+      message.channel.send(`Недостаточно поинтов. Необходимо 500 поинтов. У вас ${curxp} поинтов`)
+    };
+  }
+
+});
+
+//
+//
+//
+
+client.on("message", async message => {
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+
+  if(message.content === "z.take Бывалый") {
+    if(curxp > 750){
+      let coder = message.guild.roles.find(role => role.name === "Бывалый");
+      let member = message.member;
+      if(message.member.roles.has(coder.id)) {
+        message.channel.send(`Вы уже получали роль, ${message.author.username}`)
+      } else {
+        member.addRole(coder).catch(console.err);
+        message.channel.send(`Роль "Бывалый" выдана пользователю ${message.author.username}`)
+      };
+    } else {
+      message.channel.send(`Недостаточно поинтов. Необходимо 750 поинтов. У вас ${curxp} поинтов`)
+    };
+  }
+
+});
+
+//
+//
+//
+
+client.on("message", async message => {
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+
+  if(message.content === "z.take Интеллигент") {
+    if(curxp > 1230){
+      let coder = message.guild.roles.find(role => role.name === "Интеллигент");
+      let member = message.member;
+      if(message.member.roles.has(coder.id)) {
+        message.channel.send(`Вы уже получали роль, ${message.author.username}`)
+      } else {
+        member.addRole(coder).catch(console.err);
+        message.channel.send(`Роль "Интеллигент" выдана пользователю ${message.author.username}`)
+      };
+    } else {
+      message.channel.send(`Недостаточно поинтов. Необходимо 1230 поинтов. У вас ${curxp} поинтов`)
+    };
+  }
+
+});
+
+//
+//
+//
+
+client.on("message", async message => {
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+
+  if(message.content === "z.take Разбирающийся") {
+    if(curxp > 1510){
+      let coder = message.guild.roles.find(role => role.name === "Разбирающийся");
+      let member = message.member;
+      if(message.member.roles.has(coder.id)) {
+        message.channel.send(`Вы уже получали роль, ${message.author.username}`)
+      } else {
+        member.addRole(coder).catch(console.err);
+        message.channel.send(`Роль "Разбирающийся" выдана пользователю ${message.author.username}`)
+      };
+    } else {
+      message.channel.send(`Недостаточно поинтов. Необходимо 1510 поинтов. У вас ${curxp} поинтов`)
+    };
+  }
+
+});
+
+//
+//
+//
+
+client.on("message", async message => {
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+
+  if(message.content === "z.take Шарющий") {
+    if(curxp > 1800){
+      let coder = message.guild.roles.find(role => role.name === "Шарющий");
+      let member = message.member;
+      if(message.member.roles.has(coder.id)) {
+        message.channel.send(`Вы уже получали роль, ${message.author.username}`)
+      } else {
+        member.addRole(coder).catch(console.err);
+        message.channel.send(`Роль "Шарющий" выдана пользователю ${message.author.username}`)
+      };
+    } else {
+      message.channel.send(`Недостаточно поинтов. Необходимо 1800 поинтов. У вас ${curxp} поинтов`)
+    };
+  }
+
+});
+
+//
+//
+//
+
+client.on("message", async message => {
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+
+  if(message.content === "z.take Понимает язык") {
+    if(curxp > 2100){
+      let coder = message.guild.roles.find(role => role.name === "Понимает язык");
+      let member = message.member;
+      if(message.member.roles.has(coder.id)) {
+        message.channel.send(`Вы уже получали роль, ${message.author.username}`)
+      } else {
+        member.addRole(coder).catch(console.err);
+        message.channel.send(`Роль "Понимает язык" выдана пользователю ${message.author.username}`)
+      };
+    } else {
+      message.channel.send(`Недостаточно поинтов. Необходимо 2100 поинтов. У вас ${curxp} поинтов`)
+    };
+  }
+
+});
+
+//
+//
+//
+
+client.on("message", async message => {
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+
+  if(message.content === "z.take Профессионал") {
+    if(curxp > 2400){
+      let coder = message.guild.roles.find(role => role.name === "Профессионал");
+      let member = message.member;
+      if(message.member.roles.has(coder.id)) {
+        message.channel.send(`Вы уже получали роль, ${message.author.username}`)
+      } else {
+        member.addRole(coder).catch(console.err);
+        message.channel.send(`Роль "Профессионал" выдана пользователю ${message.author.username}`)
+      };
+    } else {
+      message.channel.send(`Недостаточно поинтов. Необходимо 2400 поинтов. У вас ${curxp} поинтов`)
+    };
+  }
+
+});
+
+//
+//
+//
+
+client.on("message", async message => {
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+
+  if(message.content === "z.take С языком на ты") {
+    if(curxp > 2800){
+      let coder = message.guild.roles.find(role => role.name === "С языком на ты");
+      let member = message.member;
+      if(message.member.roles.has(coder.id)) {
+        message.channel.send(`Вы уже получали роль, ${message.author.username}`)
+      } else {
+        member.addRole(coder).catch(console.err);
+        message.channel.send(`Роль "С языком на ты" выдана пользователю ${message.author.username}`)
+      };
+    } else {
+      message.channel.send(`Недостаточно поинтов. Необходимо 2800 поинтов. У вас ${curxp} поинтов`)
+    };
+  }
+
+});
+
+//
+//
+//
+
+client.on("message", async message => {
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+
+  if(message.content === "z.take Как пять пальцев") {
+    if(curxp > 4000){
+      let coder = message.guild.roles.find(role => role.name === "Как пять пальцев");
+      let member = message.member;
+      if(message.member.roles.has(coder.id)) {
+        message.channel.send(`Вы уже получали роль, ${message.author.username}`)
+      } else {
+        member.addRole(coder).catch(console.err);
+        message.channel.send(`Роль "С языком на ты" выдана пользователю ${message.author.username}. Поздравляем с максимальным уровнем!`)
+      };
+    } else {
+      message.channel.send(`Недостаточно поинтов. Необходимо 4000 поинтов. У вас ${curxp} поинтов`)
+    };
+  }
+
+});
+
+client.on("message", async message => {
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 1500;
+
+  if(message.content === "z.points"){
+    const embed = new Discord.RichEmbed()
+    .setTitle(`Профиль пользователя ${message.author.username}`)
+    .setColor("#00FF00")
+    .setDescription(`Ваше количество поинтов: ${curxp} поинтов.`)
+    .setFooter("Чтобы заработать больше поинтов, проявляйте больше актив на сервере");
+    message.channel.send(embed)
+  };
+});
+
 
 client.on("message", async message => {
   let verify = message.guild.roles.find(role => role.name === "пользователь");
