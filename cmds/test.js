33ebe6971e90bc8bb1f49b8c5c4d00ada07d4486
Fluiser <45771430+Fluiser@users.noreply.github.com>
2019-02-13 11:36:44 +0300

    Нипанятна

--- cmds/test.js
+++ cmds/test.js
@@ -1,20 +1,27 @@
 var Discord = require('discord.js')
-const proyden === 0;
 module.exports.run = async (argsUser, message, args,prefix) => {
+  var proyden === 0;
   let code = await Math.floor(Math.random()* 9999)
     let ggg = await message.channel.send(`Напишите в канал этот код: ${code}`)
 const collector = message.channel.createMessageCollector(m => m.author.id == message.author.id, {max: 1, time: 30000})
 collector.on('collect', async msg => {
     switch(msg.content) {
-        case code:
+        case ggg.content.join(" ").slice(27):
             await message.channel.send(`Проверка пройдена для ${message.author}: каналы открыты`)
-        proyden = 1;
         break;
         default:
             await message.channel.send('Код неверный, попробуйте снова. ')
         break;
     }
 })
+
+
+//
+//
+// страшный и не понятный код
+//
+//
+
 
 collector.on('end', async () => {
   if(proyden===0){
