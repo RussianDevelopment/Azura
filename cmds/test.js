62501390f2ceafcddf1f84b9442c0a9b55729112
Qadik <46158549+Qadik@users.noreply.github.com>
2019-02-13 11:36:59 +0300

    Update test.js

--- cmds/test.js
+++ cmds/test.js
@@ -1,27 +1,17 @@
 var Discord = require('discord.js')
+const proyden === 0;
 module.exports.run = async (argsUser, message, args,prefix) => {
-  var proyden === 0;
   let code = await Math.floor(Math.random()* 9999)
     let ggg = await message.channel.send(`Напишите в канал этот код: ${code}`)
 const collector = message.channel.createMessageCollector(m => m.author.id == message.author.id, {max: 1, time: 30000})
 collector.on('collect', async msg => {
-    switch(msg.content) {
-        case ggg.content.join(" ").slice(27):
+if(message.content === code){
             await message.channel.send(`Проверка пройдена для ${message.author}: каналы открыты`)
-        break;
-        default:
+        proyden = 1;
+  } else {
             await message.channel.send('Код неверный, попробуйте снова. ')
-        break;
     }
 })
-
-
-//
-//
-// страшный и не понятный код
-//
-//
-
 
 collector.on('end', async () => {
   if(proyden===0){
