a5ac90e3ffc2349a7a5e3e5f50efddecf1277b2b
ripple <40640834+Comingo@users.noreply.github.com>
2019-02-13 11:57:23 +0300

    Update test.js

--- cmds/test.js
+++ cmds/test.js
@@ -2,10 +2,11 @@ var Discord = require('discord.js')
 module.exports.run = async (argsUser, message, args,prefix) => {
   var proyden = 0;
   let code = await Math.floor(Math.random()* 9999)
-    let ggg = await message.channel.send(`Напишите в канал этот код: ${code}`)
+    let ggg = await message.channel.send(`Напишите в канал этот код:`);
+    let ggf = await message.channel.send(`${code}`);
 const collector = message.channel.createMessageCollector(m => m.author.id == message.author.id, {max: 1, time: 30000})
 collector.on('collect', async msg => {
-    if(msg.content === code) {
+    if(msg.content === ggf) {
             await message.channel.send(`Проверка пройдена для ${message.author}: каналы открыты`)
         proyden = 1;
 } else {
