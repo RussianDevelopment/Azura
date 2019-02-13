e2f353dd2142aca2d728bd87b9ea5eb7c1843804
ripple <40640834+Comingo@users.noreply.github.com>
2019-02-13 12:02:20 +0300

    Update test.js

--- cmds/test.js
+++ cmds/test.js
@@ -6,11 +6,10 @@ module.exports.run = async (argsUser, message, args,prefix) => {
     let ggf = await message.channel.send(`${code}`);
 const collector = message.channel.createMessageCollector(m => m.author.id == message.author.id, {max: 1, time: 30000})
 collector.on('collect', async msg => {
-    if(msg.content === ggf) {
+    switch(msg.content) {
+        case ggg.content.join(" ").slice(27):
             await message.channel.send(`Проверка пройдена для ${message.author}: каналы открыты`)
         proyden = 1;
-} else {
-            await message.channel.send('Код неверный, попробуйте снова. ')
     }
 })
 
@@ -24,7 +23,7 @@ collector.on('collect', async msg => {
 
 collector.on('end', async () => {
   if(proyden===0){
-    message.channel.send(`Время подтверждения истекло. `)
+    message.channel.send(`заменю потом `)
     }
 })
 }
