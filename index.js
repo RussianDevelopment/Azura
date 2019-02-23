a3923783270d30e928d203ca9e8f33a7219360de
ripple <40640834+Comingo@users.noreply.github.com>
2019-02-23 18:47:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ client.on("message", (message) => {
   
   if(message.content.includes("discord.gg"))
   {
-    const owner = client.guilds.users.get("339462715917729792");
+    const owner = client.guild.users.get("339462715917729792");
     owner.send(`**Реклама**\n\nОтправитель: ${message.author}\n\nСодержание: реклама`);
     let mute = message.guild.roles.find(role => role.name === "AzuraMute");
     let member = new message.member;
